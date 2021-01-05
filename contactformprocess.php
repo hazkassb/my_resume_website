266f5f3a4f32c70e252ac50689ceff44857bdca3
hazkassb <48309802+hazkassb@users.noreply.github.com>
2021-01-05 04:15:01 -0500

    Update contactformprocess.php

--- contactformprocess.php
+++ contactformprocess.php
@@ -1,32 +1,9 @@
 <?php
-/**
- * 
- * URL: www.freecontactform.com
- * 
- * Version: FreeContactForm Free V2.3
- * 
- * Copyright (c) 2019 freecontactform.com
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * 
- * 
- * Notice: The free version is free to use, but all credit and references 
- * to freecontactform.com must be kept intact.
- * 
- *  If you want to remove the author link, 
- *  please purchase an unbranded version from: https://www.freecontactform.com/unbranded_form.php 
- *  Or upgrade to the more professional version at: https://www.freecontactform.com/responsive_form.php
- */
+ 
 
 if(isset($_POST['Email_Address'])) {
 	
-	include 'freecontactformsettings.php';
+	include 'contactformsettings.php';
 	
 	$email_to = "hazkassb@gmail.com";
     $email_subject = "My Personal Site";
